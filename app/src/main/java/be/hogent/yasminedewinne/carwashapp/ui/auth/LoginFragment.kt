14f19e5df84826952409659a414d7b2c011e0df0
Yasmine  De Winne <yasmine.dewinne@student.hogent.be>
2019-11-28 14:26:50 +0100

    Basic fragments toegevoegd

---
+++ app/src/main/java/be/hogent/yasminedewinne/carwashapp/ui/auth/LoginFragment.kt
@@ -0,0 +1,26 @@
+package be.hogent.yasminedewinne.carwashapp.ui.auth
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import be.hogent.yasminedewinne.carwashapp.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.account_fragment_login, container, false)
+    }
+
+
+}
